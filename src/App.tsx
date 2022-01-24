0af7cfd8bd3fa8b3f7bcd13b52cbb8854e119bfa
Niklas Schibel <niklas.schibel@gmx.de>
2022-01-24 19:58:35 +0100

    next step: add componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import './App.css';
 import axios from "axios";
 
 export default function App() {
-    const [Animals10, setAnimals10] = useState([]);
-    const [Animals1, setAnimals1] = useState([]);
+    const [Animals10, setAnimals10] = useState<any>([]);
+    const [Animals1, setAnimals1] = useState<any>([]);
     const fetch10Characters = () => {
         axios.get("https://zoo-animal-api.herokuapp.com/animals/rand/10").then(res => {
-            console.log(res.data);
+            // console.log(res.data);
             setAnimals10(res.data);
+        }).catch(err => {
+            console.log(err.message)
+        });
+    }
+    const fetch1Characters = () => {
+        axios.get("https://zoo-animal-api.herokuapp.com/animals/rand/").then(res => {
+            console.log(res.data);
+            setAnimals1(res.data);
         }).catch(err => {
             console.log(err.message)
         });
@@ -16,6 +24,7 @@ export default function App() {
 
     useEffect(() => {
         fetch10Characters();
+        fetch1Characters();
     }, [])
 
 
@@ -24,11 +33,24 @@ export default function App() {
             <header className="App-header">
                 Zoo-API
             </header>
+            <div className="Item">
+                <div className="Id">{Animals1.id}</div>
+                <div className="Name">{Animals1.name}</div>
+                <div className="AnimalType">{Animals1.animal_type}</div>
+                <div className="Image">{Animals1.image_link}</div>
+                <img className="Image"src={Animals1.image_link} alt="Animal Image" width="160" height="200"/>
+                {/*{Object.entries(Animals1)}*/}
+                {/*{Object.keys(Animals1)}*/}
+                {/*{Object.values(Animals1)}*/}
+            </div>
             <div className="WrapperRow">
-                {Animals10.map()}
-                <div className="Item">
-
-                </div>
+                {/*{Animals10.map((animal, index) => (*/}
+                {/*    <div className='Item' key={index}>*/}
+                {/*        <div className='quantity'>*/}
+                {/*            <span className="number-size"> {animal.name} </span>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*))}*/}
             </div>
         </div>
     );
